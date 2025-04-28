3fa0afb7e780939dfd364a3c46890cc0d669a8d1
hblim2380 <dlagusqlssla@naver.com>
2025-04-29 02:42:52 +0900

    Refact: Login API 구조 변경 (#13)  - AuthService 구현하여 구조변경
     Refact #13

--- app/src/main/java/com/money/app/user/service/UserDetailsServiceImpl.java
+++ app/src/main/java/com/money/app/user/service/UserDetailsServiceImpl.java
@@ -9,17 +9,17 @@ import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor //final 필드 자동주입
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) {
+    public UserDetails loadUserByUsername(String email) { //AuthenticationManager.authenticate() 내부에서 호출
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_EMAIL_NOT_FOUND));
 
-        return org.springframework.security.core.userdetails.User
+        return org.springframework.security.core.userdetails.User //Spring Security용 UserDetails 객체 반환
                 .withUsername(user.getId())
                 .password(user.getPassword())
                 .build();
