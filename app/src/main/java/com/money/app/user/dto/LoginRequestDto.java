615aeda7aa9895dac6c8f38d304bd9212f960cac
hblim2380 <dlagusqlssla@naver.com>
2025-05-06 02:33:05 +0900

    Fix: User Login 기능 수정(email -> username) (#21)  - User Login 기능 수정(email -> username)
    Resolves #21

--- app/src/main/java/com/money/app/user/dto/LoginRequestDto.java
+++ app/src/main/java/com/money/app/user/dto/LoginRequestDto.java
@@ -1,5 +1,5 @@
 package com.money.app.user.dto;
 
-public record LoginRequestDto(String id, String password) { //User domain username에 해당하는 내용
+public record LoginRequestDto(String username, String password) {
 
 }
