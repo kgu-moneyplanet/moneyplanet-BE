3ae8c439dd39df05902ece770e8a80123eac3dd9
hblim2380 <dlagusqlssla@naver.com>
2025-05-20 03:07:46 +0900

    Feat: ABC 결정 API 구현 (#27)  - ABC 결정 API 구현  - 연동시 camel, snake 변경 설정
     Resolves #27

--- app/src/main/java/com/money/app/external/FastApiConnect.java
+++ app/src/main/java/com/money/app/external/FastApiConnect.java
@@ -1,5 +1,6 @@
 package com.money.app.external;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.money.app.module.tx.dto.DecisionResponseDto;
 import com.money.app.module.tx.dto.InputSchema;
 import com.money.app.util.exception.CustomException;
@@ -9,22 +10,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 
 @Component
 @RequiredArgsConstructor
 public class FastApiConnect {
 
-    // 이름 일치시킴
     private final RestTemplate fastApiRestTemplate;
 
     public DecisionResponseDto getAbcDecision(InputSchema input) {
-        String fastApiUrl = "http://fastapi:8000/decide";
+        String fastApiUrl = "http://moneyplanet-fastapi-server:8000/v1/decision/abc";
 
         try {
+            ObjectMapper mapper = new ObjectMapper()
+                    .registerModule(new JavaTimeModule())
+                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+            String jsonBody = mapper.writeValueAsString(input);
+            System.out.println("실제 전송 JSON:\n" + jsonBody);
+
             ResponseEntity<DecisionResponseDto> response = fastApiRestTemplate.postForEntity(
                     fastApiUrl, input, DecisionResponseDto.class
             );
             return response.getBody();
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new CustomException(ErrorCode.FASTAPI_SERVER_ERROR);
         } catch (RestClientException e) {
             throw new CustomException(ErrorCode.FASTAPI_SERVER_ERROR);
         }
